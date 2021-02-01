2970472d7e6985f1220486d98a3785772056fafa
nlombardi <nicholas.lombardi@ryerson.ca>
2021-01-31 20:06:29 -0500

    Added f-strings

--- getData.py
+++ getData.py
@@ -17,23 +17,23 @@ SPY50.info['averageDailyVolume10Day']
 
 def plotData(p1, p2='', p3='', p4='', days=10):
     # pull multiple symbols and plot against
-    data = yf.download(p1+' '+p2+' '+p3+' '+p4)
+    data = yf.download(f'{p1} {p2} {p3} {p4}')
     # data['Close'].tail(10).plot(figsize=(10,5))
     fig, ax = plt.subplots()
     # first line
-    p1=ax.plot(data.tail(10).index, data['Close'][p1].tail(10), color='red', marker='o', label=p1)
+    p1 = ax.plot(data.tail(10).index, data['Close'][p1].tail(10), color='red', marker='o', label=p1)
     ax.set_xlabel('Date', fontsize=12)
     ax.set_xticklabels(ax.get_xticks(), rotation=90, fontsize=8)
     ax.set_ylabel('Close Price', fontsize=12)
     # second line
     if p2 != '':
         ax2 = ax.twinx()
-        p2=ax2.plot(data.tail(10).index, data['Close'][p2].tail(10), color='green', marker='o', label=p2)
+        p2 = ax2.plot(data.tail(10).index, data['Close'][p2].tail(10), color='green', marker='o', label=p2)
     # format graph
-    lns = p1+p2
+    lns = p1 + p2
     labs = [l.get_label() for l in lns]
     ax.legend(lns, labs, loc=0)
     ax.grid()
     # show / save graph
     plt.show()
-    # fig.savefig(path+'GME_AMC_last10days.jpg', format='jpeg', dpi=300, bbox_inches='tight')
+    # fig.savefig(path+f'{p1}_{p2}_last{days}days.jpg', format='jpeg', dpi=300, bbox_inches='tight')
