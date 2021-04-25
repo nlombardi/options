ed9c0fefb690d35da0b428a915f51a9d95d941ab
nlombardi <nicholas.lombardi@ryerson.ca>
2021-04-25 11:17:18 -0400

    Added volume check and fixed RSI

--- Scan.py
+++ Scan.py
@@ -36,6 +36,7 @@ class Alert(Ichimoku.Analysis):
             return False
 
     def check_rsi(self, data):
+        # looks for RSI that matches a trending market for pairing with Ichimoku trends
         last_entry = round(data['rsi'][-1], 0)
         second_last_entry = round(data['rsi'][-2], 0)
         increasing = last_entry > second_last_entry
@@ -44,32 +45,46 @@ class Alert(Ichimoku.Analysis):
         four_period_avg = round(four_period.mean(), 0)
         # check if the rsi is trending up/down or hit support/resistance and bounced/fell (4 --> 1hr on 15min interval)
         if last_entry > four_period_avg and increasing \
-                or (x < 30 for x in four_period) and increasing:
+                or last_entry > 40 and (x < 40 for x in four_period) and increasing:
             return "up"
         elif last_entry > four_period_avg and decreasing \
-                or (x > 70 for x in four_period) and decreasing:
+                or last_entry < 60 and (x > 60 for x in four_period) and decreasing:
             return "down"
 
     def check_volume(self, data):
-        # Checks if the volume on the stock is greater than the average volume and over a certain threshold (ie, 50,000)
-        pass
+        # TODO: subtract first period date and last period date to get difference
+        # Checks if the volume on the stock is greater than 400,000/day
+        if interval == "15m":
+            t = 26
+        elif interval == "1h":
+            t = 7
+        else:
+            t = 26
+        t22 = t*22
+        last_day_vol = data[-t:].Volume.sum() # one day for 15min interval
+        month_avg = round(data[-t22:].Volume.sum()/22, 0)
+        if 400000 < month_avg < last_day_vol:
+            return True
+        else:
+            return False
 
     def find_entry(self):
-        # TODO: add a volume check to make sure the volume is increasing on the stock
         data = super().ichimoku(period=self.period, interval=self.interval)
         if data.empty:
             pass
         else:
             if self.check_conversion(data) == "up" \
                     and self.check_cloud(data) == "up"\
-                    and self.check_rsi(data) == "up":
+                    and self.check_rsi(data) == "up"\
+                    and self.check_volume(data):
                 print("Conversion up and price above cloud!")
                 print(f"Symbol: {self.symbol}, Period: {self.period}, Interval: {self.interval}")
                 up = {'Symbol': self.symbol, 'Period': self.period, 'Interval': self.interval, 'Buy/Sell': "BUY"}
                 return up
             elif self.check_conversion(data) == "down" \
                     and self.check_cloud(data) == "down" \
-                    and self.check_rsi(data) == "down":
+                    and self.check_rsi(data) == "down"\
+                    and self.check_volume(data):
                 print("Conversion down and price below cloud!")
                 print(f"Symbol: {self.symbol}, Period: {self.period}, Interval: {self.interval}")
                 down = {'Symbol': self.symbol, 'Period': self.period, 'Interval': self.interval, 'Buy/Sell': "SELL"}
